68fcf932f44fe9efb67fe318c693ff61b13b98a8
Eugene Barros-Pagan <p21ebarrospagan@fairfieldprep.org>
2020-09-29 14:30:30 -0400

    3-1 lab

---
+++ lab_3-1.py
@@ -0,0 +1,29 @@
+# Author: EBP 9/28/20
+
+# Question 1
+# x +=  5
+# x = x + 5
+
+# Question 2
+# y = y - 3
+# y -= 3
+
+# Question 3
+# z = 2
+# z = z  2
+
+# Question 4
+# w = w % 2
+# w %= 2
+
+# Question 5
+# t = t / 5
+# t /= 5
+
+# Question 6
+# a *= 12
+# a = a * 12
+
+# Question 7
+# i = i // 7
+# i //= 7
